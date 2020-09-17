91dffc79ceaba3818404f35838e2fa156a37f5ac
Tanvee Balhara <65037396+tanvee09@users.noreply.github.com>
2020-09-17 20:08:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = 'IamBored'; 
+var speed = 500; 
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("head").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+typeWriter();
